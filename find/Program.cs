a2d3a41de87714fba13ea0e878965e71f37a75ad
Karl Mogensen <km@bleau.dk>
2021-08-31 12:03:19 +0200

    'recoursive_bug_fix'

--- find/Program.cs
+++ find/Program.cs
@@ -8,7 +8,6 @@ namespace find
     {
         static void Main(string[] args)
         {
-
             if (args.Length < 1)
                 return;
 
@@ -23,34 +22,37 @@ namespace find
             RecoursiveFindText(toFind, new DirectoryInfo(Directory.GetCurrentDirectory()), fileExtensionsToSearch); ;
         }
 
-        private static void RecoursiveFindText(string toFind,DirectoryInfo dir,List<string> fileExtensionsToSearch) 
+        private static void RecoursiveFindText(string toFind, DirectoryInfo dir, List<string> fileExtensionsToSearch)
         {
             foreach (var file in dir.GetFiles())
             {
-                if (fileExtensionsToSearch.Count > 0
-                    && !fileExtensionsToSearch.Contains(Path.GetExtension(file.Name)))
-                    continue;
-
-                var txt = File.ReadAllText(file.FullName);
-                var matches = "";
-                var lines = txt.Split("\n");
-                for (var i = 0; i < lines.Length;i++)
-                    if (lines[i].Contains(toFind))
-                        matches += ("-- At line: " + (i + 1) + "\n");
-
-                if (!String.IsNullOrEmpty(matches))
+                if (fileExtensionsToSearch.Count == 0
+                    || fileExtensionsToSearch.Contains(Path.GetExtension(file.Name)))
                 {
-                    Console.Write($"\nFound in file: '");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write(file.Name);
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("'\n"+  matches);
+                    var txt = File.ReadAllText(file.FullName);
+                    var matches = "";
+                    var lines = txt.Split("\n");
+                    for (var i = 0; i < lines.Length; i++)
+                        if (lines[i].Contains(toFind))
+                            matches += ("-- At line: " + (i + 1) + "\n");
+
+                    if (!String.IsNullOrEmpty(matches))
+                    {
+                        Console.Write($"\nFound in file: '");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(file.Name);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("'\nFilepath: '");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(file.FullName);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("'\n" + matches);
+                    }
                 }
+            }
 
                 foreach (var subDir in dir.GetDirectories())
                     RecoursiveFindText(toFind, subDir, fileExtensionsToSearch);
-            }
-
         }
     }
 }
